e626feebd04d3331068c48526e3d2fdbe85b6d59
phpontes <ppontes95@hotmail.com>
2025-08-06 17:36:17 -0300

    resetting the form after submission

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -23,5 +23,9 @@ export class UserInputComponent {
       expectedReturn: +this.enteredExpectedReturn,
       annualInvestment: +this.enteredAnnualInvestment,
     });
+    this.enteredInitialInvestment = '0';
+    this.enteredAnnualInvestment = '0';
+    this.enteredExpectedReturn = '5';
+    this.enteredDuration = '10';
   }
 }
