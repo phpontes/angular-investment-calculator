189502cd414e55dae2873390a27c2aef01f6ccf3
phpontes <ppontes95@hotmail.com>
2025-08-07 08:09:30 -0300

    migrating to angular modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,8 @@
 import { Component } from '@angular/core';
 
-import { HeaderComponent } from './header/header.component';
-import { InvestmentResultsComponent } from './investment-results/investment-results.component';
-import { UserInputComponent } from './user-input/user-input.component';
-
 @Component({
   selector: 'app-root',
+  standalone: false,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
 })
-export class AppComponent {
-}
+export class AppComponent {}
